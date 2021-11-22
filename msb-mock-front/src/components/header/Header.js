838e8eb2b88724867b28ed81b85054b3f8ddc903
jkwcp <jk.cpwang@gmail.com>
2021-11-21 16:23:58 -0800

    initial setup of header

---
+++ msb-mock-front/src/components/header/Header.js
@@ -0,0 +1,23 @@
+import React from "react"
+import "./Header.css"
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo">
+                <span className="site-logo">
+                    <a href="/">
+                        <span id="title">
+                            MSBMockup
+                        </span>
+                    </a>
+                </span>
+            </div>
+            <div className="nav">
+
+            </div>
+        </div>
+    );
+};
+
+export default Header;
